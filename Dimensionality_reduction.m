fdbdb9e10175aec724fdedbe086ca29a3cd0c4b4
Jungjae <100228417+jungjae01eng@users.noreply.github.com>
2022-12-12 06:21:31 -0500

    Update file - simplified code

--- Dimensionality_reduction.m
+++ Dimensionality_reduction.m
@@ -1,9 +1,4 @@
-%This function takes in a data matrix Xrun, mean vector mu, 
-%eigenvector matrix V, and eigenvalues D, and dimension k. 
-%It selects the k eigenvectors corresponding to the k largest
-%eigenvalues, centers the data by subtracting mu, and projects
-%the centered data to k dimensions by multiplying by the matrix
-%of k eigenvectors.
+%This function takes in a data matrix Xrun, mean vector mu, eigenvector matrix V, and eigenvalues D, and dimension k. 
 function Xrun_reduced = Dimensionality_reduction(Xrun,mu,V,D,k)
 [~, sortedindices] = sort(-diag(D));
 Vk = V(:,sortedindices(1:k));
