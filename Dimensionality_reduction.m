1f21e1b1977aff20bfe0ef29e1bc29c151e81831
Jungjae <100228417+jungjae01eng@users.noreply.github.com>
2022-12-12 06:18:18 -0500

    Update file - export error plot as png file

--- Dimensionality_reduction.m
+++ Dimensionality_reduction.m
@@ -5,28 +5,6 @@
 %the centered data to k dimensions by multiplying by the matrix
 %of k eigenvectors.
 function Xrun_reduced = Dimensionality_reduction(Xrun,mu,V,D,k)
-%    [r, ~] = size(Xrun);
-%    Vk = V(:, 1:k);
-%    eigvals = abs(sum(D));
-
-%    for i = 1:k
-%        [~, maxi] = max(eigvals);
-%        eigvals(maxi) = 0;
-%        Vk(:, i) = V(:, maxi);
-%    end
-    
-%    Xcentered = Xrun - (ones(r, 1) * mu);
-%    Xrun_reduced = Xcentered * Vk;
-
-%[~, sortedindices] = sort(diag(D));     % diagonal matrices
-
-%i = sortedindices(k);                   % max eigenvalue
-%j = sortedindices(k-1);                 % second top eigenvalue
-%Vk = V(:,[j, i]);
-
-%Xcentered = Xrun - mu;                  % center i by subtracting the sample mean vector from each row
-%Xrun_reduced = Xcentered * Vk;
-
 [~, sortedindices] = sort(-diag(D));
 Vk = V(:,sortedindices(1:k));
 Xrun_reduced = (Xrun - mu) * Vk;
